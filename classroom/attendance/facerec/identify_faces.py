662bde6c01a3fc3718ac004be4c770a4370e0c18
Shubha Ruidas <shubharuidas123@gmail.com>
2024-09-07 01:18:51 +0530

    fix:identify_faces.py

--- classroom/attendance/facerec/identify_faces.py
+++ classroom/attendance/facerec/identify_faces.py
@@ -1,53 +1,126 @@
+from .models import Employee, Detected
 import cv2
-import os
+import pickle
 import face_recognition
+import datetime
+from cachetools import TTLCache
+from django.utils import timezone
 
 
-def click(dirName, dirID, cam):
-
-    img_counter = 0
+cache = TTLCache(maxsize=20, ttl=10)
 
 
-    # dirName = input("Please enter your name: ").upper()
-    # dirID = input("Please enter ID: ")
+def identify1(frame, name, buf, buf_length, known_conf):
+
+    if name in cache:
+        return
+    count = 0
+    for ele in buf:
+        count += ele.count(name)
     
-    DIR = f"attendance/facerec/dataset/{dirName}_{dirID}"
+    if count >= known_conf:
+        timestamp = datetime.datetime.now(tz=timezone.utc)
+        print(name, timestamp)
+        cache[name] = 'detected'
+        path = 'attendance/facerec/detected/{}_{}.jpg'.format(name, timestamp)
+        cv2.imwrite(path, frame)
+        try:
+            emp = Employee.objects.get(name=name)
+            emp.detected_set.create(time_stamp=timestamp)
+        except:
+            pass                
 
-    try:
-        os.mkdir(DIR)
-        print("Directory " , dirName ,  " Created ") 
-    except FileExistsError:
-        print("Directory " , dirName ,  " already exists")
-        img_counter = len(os.listdir(DIR))
 
 
 
+def predict(rgb_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either through knn_clf or model_path")
 
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Find face locations
+    X_face_locations = face_recognition.face_locations(rgb_frame, number_of_times_to_upsample=2)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test image
+    faces_encodings = face_recognition.face_encodings(rgb_frame, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+
+def identify_faces(video_capture):
+
+    buf_length = 10
+    known_conf = 5
+    buf = [[]] * buf_length
+    i = 0
+
+    process_this_frame = True
 
     while True:
-        ret, frame = cam.read()
-        # rgb_small_frame = frame[:, :, ::-1]
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-        # face_locations = face_recognition.face_locations(rgb_small_frame)
-        # print(len(face_locations))
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        cv2.imshow("Video", frame)
-        if not ret:
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = small_frame[:, :, ::-1]
+
+        if process_this_frame:
+            predictions = predict(rgb_frame, model_path="attendance/facerec/models/trained_model.clf")
+            # print(predictions)
+
+        process_this_frame = not process_this_frame
+
+        face_names = []
+
+        for name, (top, right, bottom, left) in predictions:
+
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            identify1(frame, name, buf, buf_length, known_conf)
+
+            face_names.append(name)
+
+        buf[i] = face_names
+        i = (i + 1) % buf_length
+
+
+        # print(buf)
+
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        k = cv2.waitKey(1)
 
-        if k%256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-            
-        elif k%256 == 32:
-            # SPACE pressed
-            img_name = f"attendance/facerec/dataset/{dirName}_{dirID}/opencv_frame_{img_counter}.png"
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            img_counter += 1
-
-    cam.release()
-
+    # Release handle to the webcam
+    video_capture.release()
     cv2.destroyAllWindows()
