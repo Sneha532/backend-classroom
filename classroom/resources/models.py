326b54d08afcb4d29c0b6ed9c4ac76e045baf43b
Shubha Ruidas <shubharuidas123@gmail.com>
2024-09-07 01:06:30 +0530

    feat:face scanning updated

--- classroom/resources/models.py
+++ classroom/resources/models.py
@@ -5,6 +5,10 @@ class Users(models.Model):
     nom = models.CharField(max_length=20)
     prenom = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
+    registrationId = models.CharField(max_length=20)
+    branch = models.CharField(max_length=20)
+    year = models.CharField(max_length=20)
+    section = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     picture = models.ImageField(upload_to='profile_pics', blank=True)
     roles = [
